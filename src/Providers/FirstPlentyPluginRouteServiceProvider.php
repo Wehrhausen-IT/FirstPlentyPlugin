9809258e9c01b9376eeba9222ef95c53796fc7d8
Florian Wehrhausen <florian.wehrhausen@web.de>
2020-01-15 03:24:22 +0100

    Fix Controllers

--- src/Providers/FirstPlentyPluginRouteServiceProvider.php
+++ src/Providers/FirstPlentyPluginRouteServiceProvider.php
@@ -12,7 +12,7 @@ class FirstPlentyPluginRouteServiceProvider extends RouteServiceProvider
     /*
       We use the get method to pass two parameters. The first parameter 'hello' defines the route. The second parameter consists of the Fully-Qualified Class Name and the @ controller method that is called when the route is called.
     */
-    $router->get('hello','FirstPlentyPlugin\Controller\ContentController@sayHello');
+    $router->get('hello','FirstPlentyPlugin\Controllers\ContentController@sayHello');
   }
 }
  ?>
