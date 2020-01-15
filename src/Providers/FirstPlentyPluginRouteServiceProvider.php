4fbf204dec3c4c7e9b1545d8a155818cb26ef389
Florian Wehrhausen <florian.wehrhausen@web.de>
2020-01-15 03:18:39 +0100

    wrong s

--- src/Providers/FirstPlentyPluginRouteServiceProvider.php
+++ src/Providers/FirstPlentyPluginRouteServiceProvider.php
@@ -10,9 +10,9 @@ class FirstPlentyPluginRouteServiceProvider extends RouteServiceProvider
   public function map(Router $router)
   {
     /*
-      We use the get method to pass two parameters. The first parameter 'hello' defines the route. The second parameter consists of the Fully-Qualified Class Name and the @ controller method that is called when the route is called. 
+      We use the get method to pass two parameters. The first parameter 'hello' defines the route. The second parameter consists of the Fully-Qualified Class Name and the @ controller method that is called when the route is called.
     */
-    $router->get('hello','FirstPlentyPlugin\Controllers\ContentController@sayHello');
+    $router->get('hello','FirstPlentyPlugin\Controller\ContentController@sayHello');
   }
 }
  ?>
